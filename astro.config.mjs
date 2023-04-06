e71249561af76766f19e4023ef9ce384a84bb010
Hector <hectordevx@gmail.com>
2023-04-06 12:58:59 -0500

    feat(config): seo and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
-import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  site: 'https://localhost:3000',
+  integrations: [tailwind(), mdx(), react(), sitemap()]
 });
